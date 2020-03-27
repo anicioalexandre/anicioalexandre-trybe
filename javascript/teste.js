71d6ca08b49eb41c66468263cda8b9d085a60078
Alexandre Anicio <alexandre.anicio94@gmail.com>
2020-03-27 13:21:22 -0300

    javascript stuff

---
+++ javascript/teste.js
@@ -0,0 +1,10 @@
+let age = 20;
+if (age > 15 && age < 20) {
+    console.log("entre 15 e 20");
+}
+else if (age >= 20 && age < 50)  {
+    console.log("entre 19 e 50");
+}
+else {
+    console.log("ok");
+}
