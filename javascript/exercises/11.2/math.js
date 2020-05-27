c25491c3ae35380d897043bb97f984f311cd6cd7
Alexandre Anicio <alexandre.anicio94@gmail.com>
2020-05-27 13:15:39 -0300

    pre aula 2

---
+++ javascript/exercises/11.2/math.js
@@ -0,0 +1,8 @@
+const somar = (a, b) => {
+  return a + b;
+}; // Função de somar mais lenta do mundo
+const subtrair = (a, b) => a - b;
+const multiplicar = (a, b) => a * b;
+const dividir = (a, b) => a / b;
+
+module.exports = { somar, subtrair, multiplicar, dividir };
