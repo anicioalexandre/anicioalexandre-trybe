6fea461c26047ab279394b8a60f458d5ca3149c3
Alexandre Anicio <alexandre.anicio94@gmail.com>
2020-06-09 14:12:18 -0300

    parte bonus

--- Front-end/block 13 - react/exercicios/src/App.js
+++ Front-end/block 13 - react/exercicios/src/App.js
@@ -1,46 +1,12 @@
 import React, { Component } from 'react';
-import Button from './Button';
-import Alert from './Alert';
+import Dropdown from './Dropdown';
+import data from './data';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      showModal: false,
-      isDisableButton: false,
-    };
-  }
-
-  changeTitle = (value) => {
-    this.setState({ title: value });
-  };
-
-  changeShowComponent = () => {
-    this.setState((state) => ({
-      showModal: !state.showModal,
-      isDisableButton: !state.isDisableButton,
-    }));
-  };
-
   render() {
     return (
       <div className="main">
-        <Button
-          content="Clique aqui"
-          isDisable={this.state.isDisableButton}
-          showComponent={this.changeShowComponent}
-          value="Título Show"
-        />
-        {this.state.showModal && (
-          <Alert hideComponent={this.changeShowComponent}>
-            {{
-              title: 'Algum título',
-              content: 'Algum conteúdo',
-              timeSeconds: 3000,
-            }}
-          </Alert>
-        )}
+        <Dropdown list={data}>a</Dropdown>
       </div>
     );
   }
---
+++ Front-end/block 13 - react/exercicios/src/data.js
@@ -0,0 +1,8 @@
+const content = [
+  {id: 1, item: 'a'},
+  {id: 2, item: 'b'},
+  {id: 3, item: 'c'},
+  {id: 4, item: 'd'},
+];
+
+export default content;
