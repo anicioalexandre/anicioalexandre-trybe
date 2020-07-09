9626ec69b8183a339955fd7aace149986f8efb7a
Alexandre Anicio <alexandre.anicio94@gmail.com>
2020-07-09 11:20:18 -0300

    exercicio 16.4

---
+++ Front-end/block 16 - react with redux/16.4/reddit_api/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Display from './components/Display';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Display />
+    </div>
+  );
+}
+
+export default App;
