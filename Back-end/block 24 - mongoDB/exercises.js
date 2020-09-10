2e6fcc7d61b4d8491ba5b9657cce71a951080dc8
Alexandre Anicio <alexandre.anicio94@gmail.com>
2020-09-10 12:04:46 -0300

    bonus

--- Back-end/block 24 - mongoDB/exercises.js
+++ Back-end/block 24 - mongoDB/exercises.js
@@ -19,7 +19,22 @@ db.movies.updateOne({ title: 'Godzilla' }, { $max: { imdbRating: 8.6 }, $set: {"
 // Exercício 10: Utilizando o operador $currentDate, crie um campo chamado lastUpdated com o tipo timestamp no filme Home Alone.
 db.movies.updateOne({ title: 'Home Alone' }, { $currentDate: { lastUpdated: { $type: 'timestamp'} } });
 // Exercício 11: Utilizando uma única operação, crie um campo chamado sequels e atribua a ele o valor 0 em todos os documentos.
-db.movies.updateMany({}, {$set: { sequels: 0 }} );
+db.movies.updateMany({}, {$set: { sequels: 0 }});
 // Exercício 12: Utilizando uma única operação, remova os campos budget e estimatedBudget em todos os documentos.
-
+db.movies.updateMany({}, {$unset: { budget: '', estimatedBudget: '' }});
 // Exercício 13: Para os filmes Batman ou Home Alone, atribua a imdbRating o valor 17 caso o valor de imdbRating seja menor que 17.
+db.movies.updateMany({ title: {$in: ['Home Alone', 'Batman'] }}, {$max: { imdbRating: 17 }});
+// Exercício 14: Remova o campo class dos documentos que possuem esse campo com o valor unknown.
+db.xmen.updateMany({ class: 'unknown' }, { $unset: { class: ''}, $currentDate: { lastUpdate: true } });
+// Exercício 15: Produza uma query que renomeie os campos de name para hero_name, e de true_name para full_name e adicione o campo power com valor 100, em todos os documentos.
+db.xmen.updateMany({}, { $rename: { name: 'hero_name', true_name: 'full_name' }, $set: { power: 100 }});
+// Exercício 16: Produza uma query onde, os mutantes class omega ou gama passam a ter seu poder de 500 somente se seu poder for menor que 500.
+db.xmen.updateMany({ class: { $in: ['omega', 'gama'] } }, { $max: { power: 500 }, $currentDate: { lastUpdate: true } });
+// Exercício 17: Produza uma query onde, os mutantes class gama passam a ter seu poder de 300 somente se seu poder for maior que 300.
+db.xmen.updateMany({ class: 'gama' }, { $min: { power: 300 }, $currentDate: { lastUpdate: true } });
+// Exercício 18: Decremente em 100 o poder dos mutantes não contêm a propriedade class.
+db.xmen.updateMany({ class: { $exists: false } }, { $inc: { power: -100 }, $currentDate: { lastUpdate: true } });
+// Exercício 19: Em apenas uma query adicione o campo area com o seguinte array como valor: [ "Students Room" ] a todos os mutantes que são Senior Staff que tenham poder acima de 100 e para todos os Junior Staff com poder acima de 200.
+db.xmen.updateMany({ $or: [{occupation: 'Senior Staff', power: { $gt: 100 }}, {occupation: 'Junior Staff', power: { $gt: 200 }}] }, { $set: { area: [ "Students Room" ] }, $currentDate: { lastUpdate: true } });
+// Exercício 20: Em apenas uma query adicione o campo áreas com [ "outside" ] a todos os Junior Staff que não tenham o campo area definido.
+db.xmen.updateMany({occupation: 'Junior Staff', area: { $exists: 0 }}, { $set: { area: [ "outside" ] }, $currentDate: { lastUpdate: true } });
